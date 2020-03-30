383cb82836de5b03f418774d2a3b2b27f3ea8d27
Marc-Antoine Ferland <maf.ferland@gmail.com>
2020-03-29 22:19:09 -0400

    :lipstick: Configure js linting

--- components/header.js
+++ components/header.js
@@ -3,7 +3,7 @@ import Link from 'next/link';
 const Header = () => (
   <div>
     <Link href="/">
-      <a>Home</a>
+      <span>Home</span>
     </Link>
   </div>
 );
--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
-const path = require("path");
-const withSass = require("@zeit/next-sass");
+const path = require('path');
+const withSass = require('@zeit/next-sass');
 
 module.exports = withSass({
-  webpack(config, options) {
-    config.resolve.alias["~"] = path.resolve(__dirname);
+  webpack(config) {
+    config.resolve.alias['~'] = path.resolve(__dirname);
     return config;
-  }
+  },
 });
