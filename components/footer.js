b7ee93953cba21e412aba0bf9ee01a8191413b32
Marc-Antoine Ferland <maf.ferland@gmail.com>
2020-03-30 23:23:01 -0400

    :sparkles: Add footer content

--- components/footer.js
+++ components/footer.js
@@ -1,5 +1,10 @@
+import './footer.scss';
+
+const year = new Date().getFullYear();
+const footerMessage = `© Built with ❤️ by maferland - ${year}`;
+
 const Footer = () => {
-  return <div className="footer" />;
+  return <div className="footer is-size-6 has-text-centered is-mono">{footerMessage}</div>;
 };
 
 export default Footer;
