197415a541c6ac016c0090eeafaacda314a635ed
Marc-Antoine Ferland <maf.ferland@gmail.com>
2020-03-30 22:29:40 -0400

    :sparkles: Add navbar styling <3

--- components/header.js
+++ components/header.js
@@ -1,12 +1,25 @@
 import Link from 'next/link';
+import './header.scss';
 
 const Header = () => (
   <div className="columns is-centered is-marginless">
     <div className="column is-11 is-flex">
-      <div>
+      <div className="nav">
         <Link href="/">
-          <span className="is-mono is-size-3">maferland</span>
+          <a className="is-mono is-size-3">maferland</a>
         </Link>
+
+        <div>
+          <Link href="/">
+            <a className="is-link is-mono is-size-5">projects</a>
+          </Link>
+          <Link href="/">
+            <a className="is-link is-mono is-size-5">blog</a>
+          </Link>
+          <Link href="/">
+            <a className="is-link is-mono is-size-5">contact</a>
+          </Link>
+        </div>
       </div>
     </div>
   </div>
