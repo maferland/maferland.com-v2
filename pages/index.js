dc6e4768eb07e76ad6dd8bbb37d5251e906a6125
Marc-Antoine Ferland <maf.ferland@gmail.com>
2020-04-03 22:23:04 -0400

    :lipstick: Add colored backgrounds

--- pages/index.js
+++ pages/index.js
@@ -5,8 +5,8 @@ import './index.scss';
 const Index = () => (
   <Layout>
     <Hero className="page fullheight" />
-    <Hero className="page fullheight" />
-    <Hero className="page fullheight" />
+    <div className="fullheight has-pink-background" />
+    <div className="fullheight has-purple-background" />
   </Layout>
 );
 
