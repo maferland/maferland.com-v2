d26df5857f14b9cd260f2bda94f6ef426e7a53e5
Marc-Antoine Ferland <maf.ferland@gmail.com>
2020-04-03 22:55:39 -0400

    :sparkles: Add scrollmagic scene pinning

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,17 @@
+import { Controller, Scene } from 'react-scrollmagic';
 import Hero from '../components/hero';
 import Layout from '../components/layout';
 import './index.scss';
 
 const Index = () => (
   <Layout>
-    <Hero className="page fullheight" />
-    <div className="fullheight has-pink-background" />
-    <div className="fullheight has-purple-background" />
+    <Controller globalSceneOptions={{ triggerHook: 'onLeave' }}>
+      <Hero className="page fullheight" />
+      <Scene duration="100%" pin>
+        <div className="scene has-pink-background" />
+      </Scene>
+      <div className="fullheight has-purple-background" />
+    </Controller>
   </Layout>
 );
 
