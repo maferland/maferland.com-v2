849f3ef121f1aa69280a4c6319908221c6db9e13
Marc-Antoine Ferland <maf.ferland@gmail.com>
2020-04-03 23:12:23 -0400

    :sparkles: Add funny footer

--- components/footer.js
+++ components/footer.js
@@ -4,7 +4,11 @@ const year = new Date().getFullYear();
 const footerMessage = `© Built with ❤️ by maferland - ${year}`;
 
 const Footer = () => {
-  return <div className="footer is-size-6 has-text-centered is-mono">{footerMessage}</div>;
+  return (
+    <div className="footer is-size-6 has-text-centered is-mono is-gradient">
+      <span>{footerMessage}</span>
+    </div>
+  );
 };
 
 export default Footer;
