3a2951ebda6f8becf516c98484fe5a98c86f1ade
Marc-Antoine Ferland <maf.ferland@gmail.com>
2020-04-05 01:29:19 -0400

    :sparkles: Add mobile friendly menu

--- components/header.js
+++ components/header.js
@@ -1,25 +1,52 @@
 import Link from 'next/link';
+import React from 'react';
 import HeaderLink from './header-link';
 import './header.scss';
 
-const Header = () => (
-  <div className="nav">
-    <Link href="/">
-      <a className="is-mono is-size-3">maferland</a>
-    </Link>
+class Header extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { active: false };
+    this.toggle = this.toggle.bind(this);
+  }
 
-    <div className="links">
-      <HeaderLink href="/portfolio">
-        <a className="link is-mono is-size-5">portfolio</a>
-      </HeaderLink>
-      <HeaderLink href="/blog">
-        <a className="link is-mono is-size-5">blog</a>
-      </HeaderLink>
-      <HeaderLink href="/contact">
-        <a className="link is-mono is-size-5">contact</a>
-      </HeaderLink>
-    </div>
-  </div>
-);
+  toggle() {
+    this.setState((prevState) => {
+      return { active: !prevState.active };
+    });
+  }
+
+  render() {
+    return (
+      <div className="nav">
+        <div className="brand">
+          <Link href="/">
+            <a className="is-mono is-size-3">maferland</a>
+          </Link>
+          {/* TODO Fix A11y issues (see .eslintrc) */}
+          <div
+            role="button"
+            className={`burger ${this.state.active ? 'active' : ''}`}
+            onClick={this.toggle}
+          >
+            <div />
+          </div>
+        </div>
+
+        <div className={`links ${this.state.active ? 'active' : ''}`}>
+          <HeaderLink href="/portfolio">
+            <a className="link is-mono is-size-5">portfolio</a>
+          </HeaderLink>
+          <HeaderLink href="/blog">
+            <a className="link is-mono is-size-5">blog</a>
+          </HeaderLink>
+          <HeaderLink href="/contact">
+            <a className="link is-mono is-size-5">contact</a>
+          </HeaderLink>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Header;
