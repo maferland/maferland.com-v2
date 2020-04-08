40639fac86529e01b23bdfd2e8bc372cdb50a84d
Marc-Antoine Ferland <maf.ferland@gmail.com>
2020-04-07 23:06:25 -0400

    :sparkles: Add placeholder about page

--- pages/about.js
+++ pages/about.js
@@ -1,6 +1,19 @@
 import Layout from '../components/layout';
+import AboutContent from '../site/content/about.json';
 import './about.scss';
 
-const About = () => <Layout>Join me</Layout>;
+const About = () => (
+  <Layout>
+    <div className="page fullheight">
+      <h1>{AboutContent.title}</h1>
+      <p>{AboutContent.description}</p>
+      {AboutContent.links.map((link) => (
+        <a key={link.title} href={link.url}>
+          {link.title}
+        </a>
+      ))}
+    </div>
+  </Layout>
+);
 
 export default About;
