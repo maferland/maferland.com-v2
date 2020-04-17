a3d92f5a82f8aff13cda574b165ea3f8b3e64eeb
Marc-Antoine Ferland <maf.ferland@gmail.com>
2020-04-16 20:30:15 -0400

    :sparkles: Add favicons + Refactor head

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import 'bulma/css/bulma.css';
+import Head from 'next/head';
 import '../styles/fonts.scss';
 import '../styles/global.scss';
 import '../styles/gradient.scss';
@@ -6,5 +7,16 @@ import '../styles/icons.scss';
 import '../styles/reset.scss';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <link rel="apple-touch-icon" sizes="180x180" href="/icons/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png" />
+        <link rel="manifest" href="/icons/site.webmanifest" />
+        <script src="https://identity.netlify.com/v1/netlify-identity-widget.js" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,3 @@
-import Head from 'next/head';
 import { Controller, Scene } from 'react-scrollmagic';
 import Hero from '../components/hero';
 import HomeSection from '../components/home-section';
@@ -7,25 +6,19 @@ import IndexContent from '../site/content/home.json';
 import './index.scss';
 
 const Index = () => (
-  <>
-    <Head>
-      <script src="https://identity.netlify.com/v1/netlify-identity-widget.js" />
-    </Head>
-
-    <Layout>
-      <Controller globalSceneOptions={{ triggerHook: 'onLeave' }}>
-        <Hero className="page fullheight" suffixes={IndexContent.suffixes} />
-        <Scene duration="100%" pin>
-          <div className="scene has-pink-background">
-            <HomeSection {...IndexContent.middle} />
-          </div>
-        </Scene>
-        <div className="fullheight has-purple-background">
-          <HomeSection {...IndexContent.end} />
+  <Layout>
+    <Controller globalSceneOptions={{ triggerHook: 'onLeave' }}>
+      <Hero className="page fullheight" suffixes={IndexContent.suffixes} />
+      <Scene duration="100%" pin>
+        <div className="scene has-pink-background">
+          <HomeSection {...IndexContent.middle} />
         </div>
-      </Controller>
-    </Layout>
-  </>
+      </Scene>
+      <div className="fullheight has-purple-background">
+        <HomeSection {...IndexContent.end} />
+      </div>
+    </Controller>
+  </Layout>
 );
 
 export default Index;
