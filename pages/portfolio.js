77a9d6eed66fc8ee6f9c543d4f9015ccbba0348e
Marc-Antoine Ferland <maf.ferland@gmail.com>
2020-04-18 22:34:18 -0400

    :lipstick: Add portfolio content + fix layout

--- pages/portfolio.js
+++ pages/portfolio.js
@@ -1,9 +1,13 @@
 import Layout from '../components/layout';
+import PortfolioContent from '../site/content/portfolio.json';
 import './portfolio.module.scss';
 
 const Portfolio = () => (
   <Layout>
-    <div className="page">Portfolio</div>
+    <div className="page">
+      <h1 className="title is-3">{PortfolioContent.title}</h1>
+      <p className="subtitle is-5">{PortfolioContent.description}</p>
+    </div>
   </Layout>
 );
 
