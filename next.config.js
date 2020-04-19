fe82b840f3d3b626a05e8ae9ed732525abab1f68
Marc-Antoine Ferland <maf.ferland@gmail.com>
2020-04-19 01:12:14 -0400

    :zap: Purge useless css

--- next.config.js
+++ next.config.js
@@ -1,16 +1,19 @@
 const path = require('path');
 const withSass = require('@zeit/next-sass');
 const withCSS = require('@zeit/next-css');
+const withPurgeCss = require('next-purgecss');
 
 module.exports = withCSS(
-  withSass({
-    webpack(config) {
-      config.resolve.alias['~'] = path.resolve(__dirname);
-      config.module.rules.push({
-        test: /\.(png|woff|woff2|eot|ttf|svg)$/,
-        loader: 'url-loader?limit=100000',
-      });
-      return config;
-    },
-  }),
+  withSass(
+    withPurgeCss({
+      webpack(config) {
+        config.resolve.alias['~'] = path.resolve(__dirname);
+        config.module.rules.push({
+          test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+          loader: 'url-loader?limit=100000',
+        });
+        return config;
+      },
+    }),
+  ),
 );
