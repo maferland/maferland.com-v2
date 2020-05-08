9eb816eb2c56759e81a9848001a5fab2e6a04410
Marc-Antoine Ferland <maf.ferland@gmail.com>
2020-05-08 17:31:05 -0400

    :bug: Tentative fix for progressive image

--- components/progressive-image.js
+++ components/progressive-image.js
@@ -2,17 +2,18 @@ import React from 'react';
 import './progressive-image.module.scss';
 
 const ProgressiveImage = ({ image, style = '', className = '' }) => {
-  const [highResImageLoaded, setHighResImageLoaded] = React.useState(false);
+  const [highResImageLoaded, setHighResImageLoaded] = React.useState(() => false);
   const lowResUrl = `${image.url}?nf_resize=fit&w=4`;
-  const highResUrl = `${image.url}?nf_resize=fit&w=1200`;
-  const handleLoad = () => setHighResImageLoaded(true);
+  const highResUrl = `${image.url}?nf_resize=smartcrop&w=1200&h=800`;
+  const handleLoad = () => {
+    setHighResImageLoaded(true);
+  };
   return (
-    <div style={style} className="is-relative">
+    <div style={{ ...style }} className="progressive-image is-relative">
       <img className={className} alt={image.alt} onLoad={() => handleLoad()} src={highResUrl} />
       <img
-        className={`${className} overlay`}
+        className={`${className} overlay ${highResImageLoaded ? 'hidden' : ''}`.trim()}
         alt={image.alt}
-        {...(highResImageLoaded && { style: { opacity: '0' } })}
         src={lowResUrl}
       />
     </div>
