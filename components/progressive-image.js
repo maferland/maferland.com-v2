4b5839484a5864ac75eebb69b82026c99c2e7bcc
Marc-Antoine Ferland <maf.ferland@gmail.com>
2020-05-08 18:09:46 -0400

    :bug: Fix cached image issue
    Cached image never fire load event

--- components/progressive-image.js
+++ components/progressive-image.js
@@ -3,16 +3,23 @@ import './progressive-image.module.scss';
 
 const ProgressiveImage = ({ image, style = '', className = '' }) => {
   const [highResImageLoaded, setHighResImageLoaded] = React.useState(() => false);
+  const imageRef = React.useRef();
   const lowResUrl = `${image.url}?nf_resize=fit&w=4`;
   const highResUrl = `${image.url}?nf_resize=smartcrop&w=1200&h=800`;
+
   const handleLoad = () => {
-    console.log('handle');
     setHighResImageLoaded(true);
   };
-  console.log('paint');
+
+  React.useEffect(() => {
+    if (imageRef.current.complete) {
+      handleLoad();
+    }
+  }, []);
+
   return (
     <div style={{ ...style }} className="progressive-image is-relative">
-      <img className={className.trim()} alt={image.alt} onLoad={handleLoad} src={highResUrl} />
+      <img className={className.trim()} alt={image.alt} onLoad={handleLoad} src={highResUrl} ref={imageRef} />
       <img
         className={`${className} overlay ${highResImageLoaded ? 'hidden' : ''}`.trim()}
         alt={image.alt}
