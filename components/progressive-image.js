5ab2e7109658d62dca255141a35d7964c91a541f
Marc-Antoine Ferland <maf.ferland@gmail.com>
2020-05-08 17:39:05 -0400

    :loud_sound: Adding debug log to progressive image loading

--- components/progressive-image.js
+++ components/progressive-image.js
@@ -6,8 +6,10 @@ const ProgressiveImage = ({ image, style = '', className = '' }) => {
   const lowResUrl = `${image.url}?nf_resize=fit&w=4`;
   const highResUrl = `${image.url}?nf_resize=smartcrop&w=1200&h=800`;
   const handleLoad = () => {
+    console.log('handle');
     setHighResImageLoaded(true);
   };
+  console.log('paint');
   return (
     <div style={{ ...style }} className="progressive-image is-relative">
       <img className={className.trim()} alt={image.alt} onLoad={() => handleLoad()} src={highResUrl} />
