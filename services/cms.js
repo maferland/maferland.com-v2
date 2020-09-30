578117efb6db1ac647862cf1e7829d2183421738
Marc-Antoine Ferland <maf.ferland@gmail.com>
2020-09-29 23:01:57 -0400

    :bug: Filter articles by date

--- services/cms.js
+++ services/cms.js
@@ -1,6 +1,9 @@
 const getPosts = async () => {
   const files = await require.context('~/site/blog', false, /\.json$/);
-  return files.keys().map((key) => files(key));
+  return files
+    .keys()
+    .map((key) => files(key))
+    .sort((a, b) => new Date(b.date) - new Date(a.date));
 };
 
 const getPost = async (slug) => {
