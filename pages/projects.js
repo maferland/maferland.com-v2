3acf7db95b4efd13b0d064d25a49c5ee66bb3b7a
Marc-Antoine Ferland <maferland@hey.com>
2020-10-17 22:52:07 -0400

    :sparkles: Display github activity

--- pages/projects.js
+++ pages/projects.js
@@ -1,14 +1,28 @@
+/** @jsx jsx */
+import { css, jsx } from '@emotion/core';
+import { UserActivityFeed } from '@maferland/github-activity';
 import BoxLink from '../components/box-link';
 import Layout from '../components/layout';
 import { getProjects } from '../services/cms';
 import ProjectsContent from '../site/content/projects.json';
+
+const grid = css`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  grid-gap: 1.5em;
+  margin-top: 1.5em;
+
+  @include touch {
+    grid-template-columns: 1fr;
+  }
+`;
 
 const Projects = ({ projects }) => (
   <Layout>
     <div className="page">
       <h1 className="title is-3">{ProjectsContent.title}</h1>
       <p className="subtitle is-5">{ProjectsContent.description}</p>
-      <div className="post-links">
+      <div css={grid}>
         {projects &&
           projects.map(({ title, description, link }) => (
             <BoxLink key={title} link={link} cta={ProjectsContent.cta}>
@@ -18,6 +32,18 @@ const Projects = ({ projects }) => (
               <p css={{ fontSize: '1.15em' }}>{description}</p>
             </BoxLink>
           ))}
+      </div>
+
+      <div css={grid}>
+        <div>
+          <h2 className="subtitle is-3">{ProjectsContent.github}</h2>
+          <UserActivityFeed username="maferland" limit={5} />
+        </div>
+
+        <div>
+          <h2 className="subtitle is-3">{ProjectsContent.spotify}</h2>
+          <div>Comming soon</div>
+        </div>
       </div>
     </div>
   </Layout>
