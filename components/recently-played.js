8a765de040c0223b75a4e87b3006c5a3d3278e02
Marc-Antoine Ferland <maferland@hey.com>
2020-10-25 21:15:50 -0400

    :sparkles: Add spotify song history

--- components/recently-played.js
+++ components/recently-played.js
@@ -1,41 +1,68 @@
 import React from 'react';
 import { useRecentlyPlayed } from '@maferland/spotify-react';
 
+const Track = ({ trackName, trackUrl, albumName, albumImageUrl }) => {
+  return (
+    <a href={trackUrl} style={{ color: '#303030', fontWeight: '700', textDecoration: 'underline' }}>
+      <img
+        src={albumImageUrl || ''}
+        alt={albumName ? `${albumName} album cover` : ''}
+        style={{
+          borderRadius: '0.25em',
+          boxShadow: '4px 2px 2px DimGray',
+          paddingTop: !albumName ? '100%' : undefined,
+          backgroundColor: 'rgb(220, 220, 220, 0.2)',
+        }}
+        height="300"
+        width="300"
+      />
+      {trackName || 'Loading...'}
+    </a>
+  );
+};
+
+const Grid = ({ children }) => (
+  <div style={{ display: 'grid', gridTemplateColumns: 'repeat(5, 1fr)', gridGap: '1em' }}>{children}</div>
+);
 const RecentlyPlayed = () => {
   // eslint-disable-next-line no-unused-vars
   const { isLoading, error, data } = useRecentlyPlayed(12166023407);
 
   if (isLoading || error || !data.items) {
     return (
-      Array(5)
-        .fill(undefined)
-        // eslint-disable-next-line react/no-array-index-key
-        .map((_, i) => <p key={i}>loading</p>)
+      <Grid>
+        {Array(10)
+          .fill(undefined)
+          .map((_, i) => (
+            // eslint-disable-next-line react/no-array-index-key
+            <Track key={i} />
+          ))}
+      </Grid>
     );
   }
 
-  const Track = ({
-    track: {
-      name,
-      album: { name: albumName, images } = { images: [] },
-      external_urls: { spotify: url },
-    },
-  }) => {
-    const medium = images.length > 0 ? images[0] : {};
-    return (
-      <a href={url}>
-        <img src={medium.url} alt={`${albumName} album cover`} />
-        {name}
-      </a>
-    );
-  };
-
   return (
-    <>
-      {data.items.map((item) => (
-        <Track key={item.played_at} {...item} />
-      ))}
-    </>
+    <Grid>
+      {data.items.map((item) => {
+        const {
+          track: {
+            name: trackName,
+            album: { name: albumName, images } = { images: [] },
+            external_urls: { spotify: trackUrl },
+          },
+        } = item;
+        const medium = images.length > 0 ? images[1] : {};
+        return (
+          <Track
+            key={item.played_at}
+            trackName={trackName}
+            trackUrl={trackUrl}
+            albumName={albumName}
+            albumImageUrl={medium.url}
+          />
+        );
+      })}
+    </Grid>
   );
 };
 
--- pages/about.js
+++ pages/about.js
@@ -1,12 +1,13 @@
 import Layout from '../components/layout';
 import ProfileImage from '../components/profile-image';
+import RecentlyPlayed from '../components/recently-played';
 import SocialLink from '../components/social-link';
 import AboutContent from '../site/content/about.json';
 import './about.module.scss';
 
 const About = () => (
   <Layout>
-    <div className="page columns is-vcentered is-centered">
+    <section className="page columns is-vcentered is-centered fullheight">
       <div className="column is-4-widescreen is-5-desktop is-6-tablet is-8-mobile is-offset-2-mobile">
         <ProfileImage
           image={AboutContent.profileImage}
@@ -23,7 +24,17 @@ const About = () => (
           ))}
         </div>
       </div>
-    </div>
+    </section>
+
+    <section className="fullheight" style={{ backgroundColor: '#f3e2ea' }}>
+      <div className="page columns is-vcentered is-centered ">
+        <div>
+          <h2 className="title is-size-3-desktop is-size-4-touch">{AboutContent.spotifyTitle}</h2>
+          <p className="subtitle is-size-4-desktop is-size-5-touch">{AboutContent.spotifyDescription}</p>
+          <RecentlyPlayed />
+        </div>
+      </div>
+    </section>
   </Layout>
 );
 
