ad67efae91b6d2415cc6cde0d590dfcf9d62781c
Marc-Antoine Ferland <maferland@hey.com>
2020-10-25 22:51:48 -0400

    :bug: Fix mobile about page

--- components/recently-played.js
+++ components/recently-played.js
@@ -1,9 +1,28 @@
-import React from 'react';
+/** @jsx jsx */
+import { jsx } from '@emotion/core';
 import { useRecentlyPlayed } from '@maferland/spotify-react';
+import facepaint from 'facepaint';
 
-const Track = ({ trackName, trackUrl, albumName, albumImageUrl }) => {
+const mq = facepaint(['@media(max-width: 1200px)']);
+
+const Track = ({ trackName, trackUrl, albumName, albumImageUrl, index }) => {
   return (
-    <a href={trackUrl} style={{ color: '#303030', fontWeight: '700', textDecoration: 'underline' }}>
+    <a
+      css={
+        index > 3
+          ? mq({
+              display: ['flex', 'none'],
+            })
+          : { display: 'flex' }
+      }
+      href={trackUrl}
+      style={{
+        color: '#303030',
+        fontWeight: '700',
+        textDecoration: 'underline',
+        flexDirection: 'column',
+      }}
+    >
       <img
         src={albumImageUrl || ''}
         alt={albumName ? `${albumName} album cover` : ''}
@@ -16,14 +35,17 @@ const Track = ({ trackName, trackUrl, albumName, albumImageUrl }) => {
         height="300"
         width="300"
       />
-      {trackName || 'Loading...'}
+      <p style={{ marginTop: '0.25em' }}>{trackName || 'Loading...'}</p>
     </a>
   );
 };
 
 const Grid = ({ children }) => (
-  <div style={{ display: 'grid', gridTemplateColumns: 'repeat(5, 1fr)', gridGap: '1em' }}>{children}</div>
+  <div css={mq({ display: 'grid', gridTemplateColumns: ['repeat(5, 1fr)', 'repeat(2,1fr)'], gridGap: '1em' })}>
+    {children}
+  </div>
 );
+
 const RecentlyPlayed = () => {
   // eslint-disable-next-line no-unused-vars
   const { isLoading, error, data } = useRecentlyPlayed(12166023407);
@@ -35,7 +57,7 @@ const RecentlyPlayed = () => {
           .fill(undefined)
           .map((_, i) => (
             // eslint-disable-next-line react/no-array-index-key
-            <Track key={i} />
+            <Track key={i} index={i} />
           ))}
       </Grid>
     );
@@ -43,7 +65,7 @@ const RecentlyPlayed = () => {
 
   return (
     <Grid>
-      {data.items.map((item) => {
+      {data.items.map((item, i) => {
         const {
           track: {
             name: trackName,
@@ -59,6 +81,7 @@ const RecentlyPlayed = () => {
             trackUrl={trackUrl}
             albumName={albumName}
             albumImageUrl={medium.url}
+            index={i}
           />
         );
       })}
--- pages/about.js
+++ pages/about.js
@@ -1,3 +1,6 @@
+/** @jsx jsx */
+import { css, jsx } from '@emotion/core';
+import facepaint from 'facepaint';
 import Arrow from '../components/arrow';
 import Layout from '../components/layout';
 import ProfileImage from '../components/profile-image';
@@ -6,17 +9,25 @@ import SocialLink from '../components/social-link';
 import AboutContent from '../site/content/about.json';
 import './about.module.scss';
 
+const mq = facepaint(['@media(max-width: 1200px)']);
+
+const sectionStyle = css`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
 const About = () => (
   <Layout>
-    <section className="page columns is-vcentered is-centered fullheight">
-      <div className="column is-4-widescreen is-5-desktop is-6-tablet is-8-mobile is-offset-2-mobile">
+    <section className="page fullheight" css={mq(sectionStyle, { flexDirection: ['row', 'column'] })}>
+      <div className="column is-4-widescreen is-5-desktop is-6-tablet is-8-mobile">
         <ProfileImage
           image={AboutContent.profileImage}
           underImage={AboutContent.underProfileImage}
           alt={AboutContent.profileAlt}
         />
       </div>
-      <div className="column is-6 is-offset-1 has-text-centered-desktop">
+      <div className="column is-6 is-offset-1 has-text-centered-desktop" css={mq({ flex: [1, 0] })}>
         <h1 className="title is-size-3-desktop is-size-4-touch">{AboutContent.title}</h1>
         <p className="subtitle is-size-4-desktop is-size-5-touch">{AboutContent.description}</p>
         <div className="is-relative has-text-centered">
@@ -25,15 +36,29 @@ const About = () => (
           ))}
         </div>
       </div>
-      <div style={{ position: 'absolute', bottom: 0, width: '100%', textAlign: 'center', paddingBottom: '1.5em' }}>
+      <div
+        style={{
+          position: 'absolute',
+          bottom: 0,
+          display: 'flex',
+          flexDirection: 'row',
+          justifyContent: 'center',
+          alignItems: 'center',
+          paddingBottom: '1.5em',
+        }}
+      >
         <a href="#spotify">
           <Arrow />
         </a>
       </div>
     </section>
 
-    <section className="fullheight" style={{ backgroundColor: '#f3e2ea' }} id="spotify">
-      <div className="page columns is-vcentered is-centered ">
+    <section
+      className="fullheight"
+      css={mq({ backgroundColor: '#f3e2ea', display: 'flex', justifyContent: 'center', alignItems: 'center' })}
+      id="spotify"
+    >
+      <div className="page columns is-mobile is-vcentered is-centered is-marginless">
         <div>
           <h2 className="title is-size-3-desktop is-size-4-touch">{AboutContent.spotifyTitle}</h2>
           <p className="subtitle is-size-4-desktop is-size-5-touch">{AboutContent.spotifyDescription}</p>
