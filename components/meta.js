6484ba33ea8036155bf3ce0fb5a42306cd372698
Marc-Antoine Ferland <maferland@hey.com>
2021-02-25 11:49:48 -0500

    feat: add twitter image meta

--- components/meta.js
+++ components/meta.js
@@ -7,9 +7,10 @@ const Meta = ({ title, description, thumbnail }) => {
       <meta property="og:site_name" content={title} key="og:site_name" />
       <meta property="og:title" content={title} key="og:title" />
       <meta property="og:description" content={description} key="og:description" />
+      <meta property="og:image" content={thumbnail || '/img/share.png'} key="og:image" />
       <meta property="twitter:title" content={title} key="twitter:title" />
       <meta property="twitter:description" content={description} key="og:description" />
-      <meta property="og:image" content={thumbnail || '/img/share.png'} key="og:image" />
+      <meta property="twitter:image" content={thumbnail || '/img/share.png'} key="og:image" />
     </Head>
   );
 };
