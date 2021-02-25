891f1ff60c087e30baab7dad015153b7f0fd9ab9
Marc-Antoine Ferland <maferland@hey.com>
2021-02-24 20:31:28 -0500

    fix: default thumbnail image

--- components/meta.js
+++ components/meta.js
@@ -1,15 +1,17 @@
 import Head from 'next/head';
 
-const Meta = ({ title, description, thumbnail = '/img/share.png' }) => (
-  <Head>
-    <title>{title}</title>
-    <meta property="og:site_name" content={title} key="og:site_name" />
-    <meta property="og:title" content={title} key="og:title" />
-    <meta property="og:description" content={description} key="og:description" />
-    <meta property="twitter:title" content={title} key="twitter:title" />
-    <meta property="twitter:description" content={description} key="og:description" />
-    <meta property="og:image" content={thumbnail} />
-  </Head>
-);
+const Meta = ({ title, description, thumbnail }) => {
+  return (
+    <Head>
+      <title>{title}</title>
+      <meta property="og:site_name" content={title} key="og:site_name" />
+      <meta property="og:title" content={title} key="og:title" />
+      <meta property="og:description" content={description} key="og:description" />
+      <meta property="twitter:title" content={title} key="twitter:title" />
+      <meta property="twitter:description" content={description} key="og:description" />
+      <meta property="og:image" content={thumbnail || '/img/share.png'} key="og:image" />
+    </Head>
+  );
+};
 
 export default Meta;
