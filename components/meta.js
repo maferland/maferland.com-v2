92d2302139a404e947b78c7bb54f0914d3553681
Marc-Antoine Ferland <maferland@hey.com>
2021-02-25 11:56:41 -0500

    fix: add required meta

--- components/meta.js
+++ components/meta.js
@@ -9,7 +9,7 @@ const Meta = ({ title, description, thumbnail }) => {
       <meta property="og:description" content={description} key="og:description" />
       <meta property="og:image" content={thumbnail || '/img/share.png'} key="og:image" />
       <meta name="twitter:card" content="summary" key="twitter:card" />
-      <meta name="twitter:author" content="@ma_ferland" key="twitter:card" />
+      <meta name="twitter:site" content="@ma_ferland" key="twitter:site" />
       <meta property="twitter:title" content={title} key="twitter:title" />
       <meta property="twitter:description" content={description} key="og:description" />
       <meta property="twitter:image" content={thumbnail || '/img/share.png'} key="twitter:image" />
