91354b1b122d34201004bc17a9fa0d00a4b6481f
Marc-Antoine Ferland <maferland@hey.com>
2021-02-25 11:50:11 -0500

    fix: twitter:image meta key

--- components/meta.js
+++ components/meta.js
@@ -10,7 +10,7 @@ const Meta = ({ title, description, thumbnail }) => {
       <meta property="og:image" content={thumbnail || '/img/share.png'} key="og:image" />
       <meta property="twitter:title" content={title} key="twitter:title" />
       <meta property="twitter:description" content={description} key="og:description" />
-      <meta property="twitter:image" content={thumbnail || '/img/share.png'} key="og:image" />
+      <meta property="twitter:image" content={thumbnail || '/img/share.png'} key="twitter:image" />
     </Head>
   );
 };
