5ebc484ce3a8dbe36f748202770a2e9ded552235
Marc-Antoine Ferland <maferland@hey.com>
2021-02-25 12:07:15 -0500

    fix: image meta url

--- components/meta.js
+++ components/meta.js
@@ -11,12 +11,12 @@ const Meta = ({ title, description, thumbnail, alt, type }) => {
       <MetaWrapper property="og:site_name" content={title} />
       <MetaWrapper property="og:title" content={title} />
       <MetaWrapper property="og:description" content={description} />
-      <MetaWrapper property="og:image" content={thumbnail || '/img/share.png'} />
+      <MetaWrapper property="og:image" content={`https://maferland.com/${thumbnail || '/img/share.png'}`} />
       <MetaWrapper property="twitter:card" content="summary" />
       <MetaWrapper property="twitter:site" content="@ma_ferland" />
       <MetaWrapper property="twitter:title" content={title} />
       <MetaWrapper property="twitter:description" content={description} />
-      <MetaWrapper property="twitter:image" content={thumbnail || '/img/share.png'} />
+      <MetaWrapper property="twitter:image" content={`https://maferland.com/${thumbnail || '/img/share.png'}`} />
       {alt && <MetaWrapper property="twitter:image:alt" content={alt} />}
     </Head>
   );
