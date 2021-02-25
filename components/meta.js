35755a45b3c37ed4f9be9025b15b84a3cfe0908c
Marc-Antoine Ferland <maferland@hey.com>
2021-02-25 11:54:09 -0500

    feat: configure twitter card

--- components/meta.js
+++ components/meta.js
@@ -8,6 +8,8 @@ const Meta = ({ title, description, thumbnail }) => {
       <meta property="og:title" content={title} key="og:title" />
       <meta property="og:description" content={description} key="og:description" />
       <meta property="og:image" content={thumbnail || '/img/share.png'} key="og:image" />
+      <meta name="twitter:card" content="summary" key="twitter:card" />
+      <meta name="twitter:author" content="@ma_ferland" key="twitter:card" />
       <meta property="twitter:title" content={title} key="twitter:title" />
       <meta property="twitter:description" content={description} key="og:description" />
       <meta property="twitter:image" content={thumbnail || '/img/share.png'} key="twitter:image" />
