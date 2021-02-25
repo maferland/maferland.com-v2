222120d52998e7ed699cb53fe91a62c42ef97bb1
Marc-Antoine Ferland <maferland@hey.com>
2021-02-25 12:04:18 -0500

    fix: meta wrapper props

--- components/meta.js
+++ components/meta.js
@@ -1,7 +1,9 @@
 import Head from 'next/head';
 
 const Meta = ({ title, description, thumbnail, alt, type }) => {
-  const MetaWrapper = ({ property, content }) => <meta property={property} content={content} key={property} />;
+  const MetaWrapper = ({ property, content }) => (
+    <meta property={property} name="property" content={content} key={property} />
+  );
   return (
     <Head>
       <title>{title}</title>
@@ -10,8 +12,8 @@ const Meta = ({ title, description, thumbnail, alt, type }) => {
       <MetaWrapper property="og:title" content={title} />
       <MetaWrapper property="og:description" content={description} />
       <MetaWrapper property="og:image" content={thumbnail || '/img/share.png'} />
-      <MetaWrapper name="twitter:card" content="summary" />
-      <MetaWrapper name="twitter:site" content="@ma_ferland" />
+      <MetaWrapper property="twitter:card" content="summary" />
+      <MetaWrapper property="twitter:site" content="@ma_ferland" />
       <MetaWrapper property="twitter:title" content={title} />
       <MetaWrapper property="twitter:description" content={description} />
       <MetaWrapper property="twitter:image" content={thumbnail || '/img/share.png'} />
