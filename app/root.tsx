22c7e6970db01af191b574dc4af0cdb92bc7e363
Marc-Antoine Ferland <me@maferland.com>
2022-02-16 21:47:25 -0500

    feat(): remix + tailwind initial commit

---
+++ app/root.tsx
@@ -0,0 +1,69 @@
+import type {MetaFunction} from 'remix'
+import {
+  Links,
+  LiveReload,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+} from 'remix'
+import reset from '~/styles/reset.css'
+import tailwind from './tailwind.css'
+
+export function links() {
+  return [
+    {rel: 'stylesheet', href: reset},
+    {rel: 'stylesheet', href: tailwind},
+  ]
+}
+
+export const meta: MetaFunction = () => {
+  return {title: 'Marc-Antoine Ferland'}
+}
+
+function Document({
+  children,
+  title,
+}: {
+  children: React.ReactNode
+  title?: string
+}) {
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta
+          name="viewport"
+          content="width=device-width,initial-scale=1"
+        />{' '}
+        {title ? <title>{title}</title> : null}
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        {children}
+        <ScrollRestoration />
+        <Scripts />
+        {process.env.NODE_ENV === 'development' && <LiveReload />}
+      </body>
+    </html>
+  )
+}
+
+function Layout({children}: React.PropsWithChildren<{}>) {
+  return (
+    <div>
+      <main>{children}</main>
+    </div>
+  )
+}
+
+export default function App() {
+  return (
+    <Document>
+      <Layout>
+        <Outlet />
+      </Layout>
+    </Document>
+  )
+}
---
+++ app/routes/index.tsx
@@ -0,0 +1,3 @@
+export default function Index() {
+  return <h1 className="text-red-400">Hello</h1>
+}
