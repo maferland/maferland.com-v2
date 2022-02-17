141a38b8501ffc27fdd273b685c509a636fea769
Marc-Antoine Ferland <me@maferland.com>
2022-02-16 23:12:03 -0500

    feat(): install radix color in tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,80 @@
+const {
+  mauve,
+  tomato,
+  red,
+  crimson,
+  pink,
+  plum,
+  purple,
+  violet,
+  indigo,
+  blue,
+  sky,
+  cyan,
+  teal,
+  mint,
+  green,
+  grass,
+  lime,
+  yellow,
+  amber,
+  orange,
+} = require('@radix-ui/colors')
+
+const matches = {
+  1: '1200',
+  2: '1100',
+  3: '1000',
+  4: '900',
+  5: '800',
+  6: '700',
+  7: '600',
+  8: '500',
+  9: '400',
+  10: '300',
+  11: '200',
+  12: '100',
+}
+
+const radixToTailwind = (color) => {
+  return Object.entries(color).reduce((acc, [key, value]) => {
+    const [, name, intenstiy] = /^(\D*)(\d+)$/.exec(key)
+    const newKey = `${name}-${matches[intenstiy]}`
+    acc[newKey] = value
+    return acc
+  }, {})
+}
+
+const colors = radixToTailwind({
+  ...tomato,
+  ...red,
+  ...crimson,
+  ...pink,
+  ...plum,
+  ...purple,
+  ...violet,
+  ...indigo,
+  ...blue,
+  ...sky,
+  ...cyan,
+  ...teal,
+  ...mint,
+  ...green,
+  ...grass,
+  ...lime,
+  ...yellow,
+  ...amber,
+  ...orange,
+  ...mauve,
+})
+console.log(colors)
+
 module.exports = {
   content: ['./app/**/*.{ts,tsx,jsx,js}'],
   theme: {
-    extend: {},
+    extend: {
+      colors,
+    },
   },
   variants: {},
   plugins: [],
