8721f13dd6bb68cc66dbb15b9d6e70afb4d79416
Marc-Antoine Ferland <me@maferland.com>
2022-02-16 22:15:06 -0500

    feat(): apply linter/formatter

--- app/root.tsx
+++ app/root.tsx
@@ -1,42 +1,25 @@
-import type {MetaFunction} from 'remix'
-import {
-  Links,
-  LiveReload,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration,
-} from 'remix'
+import type { MetaFunction } from 'remix'
+import { Links, LiveReload, Meta, Outlet, Scripts, ScrollRestoration } from 'remix'
 import reset from '~/styles/reset.css'
 import tailwind from './tailwind.css'
 
 export function links() {
   return [
-    {rel: 'stylesheet', href: reset},
-    {rel: 'stylesheet', href: tailwind},
+    { rel: 'stylesheet', href: reset },
+    { rel: 'stylesheet', href: tailwind },
   ]
 }
 
 export const meta: MetaFunction = () => {
-  return {title: 'Marc-Antoine Ferland'}
+  return { title: 'Marc-Antoine Ferland' }
 }
 
-function Document({
-  children,
-  title,
-}: {
-  children: React.ReactNode
-  title?: string
-}) {
+function Document({ children, title }: { children: React.ReactNode; title?: string }) {
   return (
     <html lang="en">
       <head>
         <meta charSet="utf-8" />
-        <meta
-          name="viewport"
-          content="width=device-width,initial-scale=1"
-        />{' '}
-        {title ? <title>{title}</title> : null}
+        <meta name="viewport" content="width=device-width,initial-scale=1" /> {title ? <title>{title}</title> : null}
         <Meta />
         <Links />
       </head>
@@ -50,7 +33,7 @@ function Document({
   )
 }
 
-function Layout({children}: React.PropsWithChildren<{}>) {
+function Layout({ children }: React.PropsWithChildren<{}>) {
   return (
     <div>
       <main>{children}</main>
