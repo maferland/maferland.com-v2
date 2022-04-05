7a3af58e81ee3683208e927e9e7386e9f44b2636
Marc-Antoine Ferland <me@maferland.com>
2022-04-04 23:20:00 -0400

    chore(): add placeholder content

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,3 +1,45 @@
+const Box: React.FC = ({ children }) => (
+  <div className="h-full shadow-md drop-shadow-md bg-purple-900 p-2 rounded-md">{children}</div>
+)
+
 export default function Index() {
-  return <h1 className="text-red-400">Hello</h1>
+  return (
+    <div className="flex flex-col gap-10 px-16">
+      <h1 className="text-slate-100 text-6xl">Welcome!</h1>
+
+      <h2 className="text-slate-100 text-4xl">Podcasts</h2>
+      <div className="grid gap-4 grid-cols-1">
+        <iframe
+          style={{ borderRadius: '12px' }}
+          src="https://open.spotify.com/embed/episode/6tjggH5MFxA8sHIj3VuUvK?utm_source=generator&theme=0"
+          width="100%"
+          height="152"
+          frameBorder="0"
+          allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+        ></iframe>
+        <iframe
+          style={{ borderRadius: '12px' }}
+          src="https://open.spotify.com/embed/episode/6tjggH5MFxA8sHIj3VuUvK?utm_source=generator&theme=0"
+          width="100%"
+          height="152"
+          frameBorder="0"
+          allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+        ></iframe>
+      </div>
+
+      <h2 className="text-slate-100 text-4xl">Post</h2>
+      <div className="grid gap-4 grid-cols-3">
+        <Box>Ur dur post #1</Box>
+        <Box>Ur dur post #2</Box>
+        <Box>Ur dur post #3</Box>
+      </div>
+
+      <h2 className="text-slate-100 text-4xl">Projects</h2>
+      <div className="grid gap-4 grid-cols-3">
+        <Box>Ur dur project #1</Box>
+        <Box>Ur dur project #2</Box>
+        <Box>Ur dur project #3</Box>
+      </div>
+    </div>
+  )
 }
