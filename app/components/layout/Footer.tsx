53cd1e66eae9956a7c9b9360c0a53fb63bb38e28
Marc-Antoine Ferland <me@maferland.com>
2022-04-06 00:07:50 -0400

    feat(): adapt footer

--- app/components/layout/Footer.tsx
+++ app/components/layout/Footer.tsx
@@ -1,7 +1,7 @@
 const year = new Date().getFullYear()
 const content = `© Built with <3 by Marc-Antoine Ferland - ${year}`
 const Footer: React.FC = () => (
-  <footer className="py-4 text-center gradient mix-blend-difference text-black font-mono">{content}</footer>
+  <footer className="py-3 text-xs text-center gradient text-violet-900 font-mono">{content}</footer>
 )
 
 export default Footer
