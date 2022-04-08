a11e730984a7194fae4cdf455c88f06193c0321e
Marc-Antoine Ferland <me@maferland.com>
2022-04-08 08:46:18 -0400

    feat(): wip - blog index

--- app/routes/blog/index.tsx
+++ app/routes/blog/index.tsx
@@ -1,7 +1,65 @@
+import Title from '~/components/typography/title'
+import { json, Link, useLoaderData } from 'remix'
+
+import * as postA from './im-joining-capdesk.mdx'
+import Subtitle from '~/components/typography/subtitle'
+import Paragraph from '~/components/typography/paragraph'
+
+type PostAttributes = {
+  meta: {
+    title: string
+    description: string
+    date: string
+    image: { url: string; alt: string }
+  }
+}
+
+type Post = { slug: string } & PostAttributes['meta']
+
+function postFromModule({ filename, attributes }: { filename: string; attributes: PostAttributes }): Post {
+  return {
+    slug: filename.replace(/\.mdx?$/, ''),
+    ...attributes.meta,
+  }
+}
+
+export async function loader() {
+  // Return metadata about each of the posts for display on the index page.
+  // Referencing the posts here instead of in the Index component down below
+  // lets us avoid bundling the actual posts themselves in the bundle for the
+  // index page.
+  return json([postFromModule(postA)])
+}
+
+const formatDate = (date: string) =>
+  new Intl.DateTimeFormat(undefined, {
+    month: 'short',
+    year: 'numeric',
+  }).format(new Date(date))
+
 export default function Index() {
+  const posts = useLoaderData<Post[]>()
+
   return (
-    <div className="flex flex-col gap-10 px-16">
-      <h1 className="text-slate-100 text-6xl">Blog!</h1>
-    </div>
+    <>
+      <Title>Blog</Title>
+
+      <div className="grid grid-cols-4 gap-4">
+        {posts.map((post) => (
+          <div key={post.slug} className="aspect-square box-shadow p-4 bg-purple-800 rounded-sm">
+            <Link to={post.slug}>
+              <div className="h-full flex flex-col justify-between">
+                <span className="font-mono">{formatDate(posts[0].date)}</span>
+                <div>
+                  <Subtitle>{post.title}</Subtitle>
+                  <Paragraph>{post.description}</Paragraph>
+                </div>
+                <div></div>
+              </div>
+            </Link>
+          </div>
+        ))}
+      </div>
+    </>
   )
 }
