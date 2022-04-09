399d03ef6a2d243c1e24544abc5dd14d89b5f2ff
Marc-Antoine Ferland <me@maferland.com>
2022-04-08 23:49:06 -0400

    feat(): add WIP message

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -5,41 +5,7 @@ const Box: React.FC = ({ children }) => (
 export default function Index() {
   return (
     <>
-      <h1 className="text-slate-100 text-6xl">Welcome!</h1>
-
-      <h2 className="text-slate-100 text-4xl">Podcasts</h2>
-      <div className="grid gap-4 grid-cols-1">
-        <iframe
-          style={{ borderRadius: '12px' }}
-          src="https://open.spotify.com/embed/episode/6tjggH5MFxA8sHIj3VuUvK?utm_source=generator&theme=0"
-          width="100%"
-          height="152"
-          frameBorder="0"
-          allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
-        ></iframe>
-        <iframe
-          style={{ borderRadius: '12px' }}
-          src="https://open.spotify.com/embed/episode/6tjggH5MFxA8sHIj3VuUvK?utm_source=generator&theme=0"
-          width="100%"
-          height="152"
-          frameBorder="0"
-          allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
-        ></iframe>
-      </div>
-
-      <h2 className="text-slate-100 text-4xl">Post</h2>
-      <div className="grid gap-4 grid-cols-3">
-        <Box>Ur dur post #1</Box>
-        <Box>Ur dur post #2</Box>
-        <Box>Ur dur post #3</Box>
-      </div>
-
-      <h2 className="text-slate-100 text-4xl">Projects</h2>
-      <div className="grid gap-4 grid-cols-3">
-        <Box>Ur dur project #1</Box>
-        <Box>Ur dur project #2</Box>
-        <Box>Ur dur project #3</Box>
-      </div>
+      <h1 className="text-slate-100 text-6xl">Work in progress 👷‍♂️</h1>
     </>
   )
 }
