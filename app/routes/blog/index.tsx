ccf15cb6feb7e8bb0c99678cddc46287be447d72
Marc-Antoine Ferland <me@maferland.com>
2022-04-12 23:05:38 -0400

    feat(): improve blog card

--- app/routes/blog/index.tsx
+++ app/routes/blog/index.tsx
@@ -1,5 +1,6 @@
 import Title from '~/components/typography/title'
 import { json, Link, useLoaderData } from 'remix'
+import { FiArrowRight } from 'react-icons/fi'
 
 import * as postA from './__posts/im-joining-capdesk.mdx'
 import Subtitle from '~/components/typography/subtitle'
@@ -44,17 +45,25 @@ export default function Index() {
     <>
       <Title>Blog</Title>
 
-      <div className="grid grid-cols-4 gap-4">
+      <div className="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
         {posts.map((post) => (
-          <div key={post.slug} className="aspect-square box-shadow p-4 bg-purple-800 rounded-sm">
+          <div
+            key={post.slug}
+            className="group aspect-square box-shadow p-4 bg-purple-800 rounded-sm hover:drop-shadow-xl hover:shadow-xl transition ease-in-all duration-700"
+          >
             <Link to={post.slug}>
               <div className="h-full flex flex-col justify-between">
-                <span className="font-mono">{formatDate(posts[0].date)}</span>
+                <div className="flex flex-row justify-between">
+                  <span className="font-mono">{formatDate(posts[0].date)}</span>
+                  <span className="bg-violet-500 text-violet-50 px-1 rounded-sm border border-violet-600">article</span>
+                </div>
                 <div>
                   <Subtitle>{post.title}</Subtitle>
                   <Paragraph>{post.description}</Paragraph>
                 </div>
-                <div></div>
+                <div className="flex flex-row justify-end">
+                  <FiArrowRight className="text-violet-500 group-hover:animate-pulse" size={25} />
+                </div>
               </div>
             </Link>
           </div>
